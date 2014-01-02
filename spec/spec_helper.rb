571870c4cc6631c84d10bc93fd36f83a4e248119
Marnen Laibow-Koser <marnen@marnen.org>
2014-01-02 15:23:41 -0500

    Set up RSpec as recommended at http://viget.com/extend/rails-engine-testing-with-rspec-capybara-and-factorygirl [#63162636]

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@ ENV["RAILS_ENV"] ||= 'test'
 require 'coveralls'
 Coveralls.wear!('rails')
 
-require File.expand_path("../../config/environment", __FILE__)
+require File.expand_path("../dummy/config/environment", __FILE__)
 require 'rspec/rails'
 require 'sidekiq/testing'
 
