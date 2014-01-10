148d8756dc93df7cd80132080612cdc30bcbfc1e
Marnen Laibow-Koser <marnen@marnen.org>
2014-01-10 14:06:34 -0500

    Fix path for test files. [#63162636]

--- spec/uploaders/project_uploader_spec.rb
+++ spec/uploaders/project_uploader_spec.rb
@@ -7,7 +7,7 @@ describe ProjectUploader do
   before do
     ProjectUploader.enable_processing = true
     @uploader = ProjectUploader.new(project, :uploaded_image)
-    @uploader.store!(File.open("#{Rails.root}/spec/fixtures/image.png"))
+    @uploader.store!(File.open(File.expand_path '../../fixtures/image.png', __FILE__))
   end
 
   after do
