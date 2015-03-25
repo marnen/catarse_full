ec777383be21b8767c067ba82e2f1b6942eb6ce6
Marnen Laibow-Koser <marnen@marnen.org>
2015-03-25 13:29:53 -0400

    Get host application to recognize Catarse migrations. [#89216430]

--- lib/catarse_full/engine.rb
+++ lib/catarse_full/engine.rb
@@ -59,6 +59,14 @@ module Catarse
       :update_observer, :project_observer, :payment_notification_observer
     ]
 
+    # See http://pivotallabs.com/leave-your-migrations-in-your-rails-engines/ .
+    # Rails 4 syntax will be different.
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        app.config.paths["db/migrate"] += config.paths["db/migrate"].expanded
+      end
+    end
+
     initializer 'assets' do |app|
       config = app.config
 
